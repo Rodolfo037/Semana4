fef7b702d411427291bdf01b9fecac48eac31697
Rodolfo037 <IsengardxD@gmail.com>
2026-06-14 16:17:22 -0500

    Subiendo el ejercicio 2 (suma de enteros)

---
+++ semana4.ejercicio2.py
@@ -0,0 +1,11 @@
+#Crea una función que reciba dos enteros y retorne su suma. 
+# Luego muestra el resultado en el programa principal.
+
+def suma(a, b):
+    resultado = a + b
+    return resultado
+total = suma(3,4)
+print(f"La suma es: {total}")
+
+
+
