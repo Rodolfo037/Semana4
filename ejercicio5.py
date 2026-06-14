46376fdb729dafa39132a277d140e9059cf4f38f
Rodolfo037 <IsengardxD@gmail.com>
2026-06-14 16:23:19 -0500

    ejercicio5 (Tabla de multiplicar)

---
+++ ejercicio5.py
@@ -0,0 +1,12 @@
+# #Crea una función void que reciba un número y muestre 
+# # su tabla de multiplicar completa del 1 al 10.
+def tabla (numero):
+    for i in range (1,11):
+        resultado = numero * i
+        print (f"{numero} * {i} = {resultado}")
+               
+mult = tabla (8)
+     
+  
+
+
