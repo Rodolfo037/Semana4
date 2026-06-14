9476a4cd1d9f7b9ac160f4fc632f3637c3cdffc7
Rodolfo037 <IsengardxD@gmail.com>
2026-06-14 14:13:45 -0500

    Cambiando de 2 variables a 1, ya que el cuadrado tiene todos los lados iguales

--- cuadrado.py
+++ cuadrado.py
@@ -1,18 +1,14 @@
 # Demuestra que una variable creada dentro de una función no es accesible desde fuera. Calcula el área de un cuadrado
 
 # Función que calcula el área del cuadrado
-def cuadrado(l1, l2):
-    area = l1 * l2
-    return area
+def area_del_cuadrado(lado):
+    resultado = lado * lado
+    return resultado
+area = area_del_cuadrado(6)
+print(f"El area del cuadrado es: {area}")
 
-#Pide al usuario ingresa los lados del cuadrado
-print("Hallar el área del cuadrado.")
-resu1 = float(input(f"Ingresa un lado: "))
-resu2 = float(input(f"Ingresa un lado: "))
-resu3 = cuadrado(resu1, resu2)
-print(f"El área del cuadrado es {resu3}")
 
 try:
-    print(area)
+    print(resultado)
 except NameError:
-    print("Demostrado, intentaste llamar a la variable 'area' que esta dentro de una función pero esta no es accesible.")
+    print("Demostrado, intentaste llamar a la variable 'resultado' que esta dentro de una función pero esta no es accesible.")
