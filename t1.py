23b10a66cbe3aecf7b70bd8eb21f99c40f0c7384
Antonio <aquice.valentin@gmail.com>
2026-06-24 22:54:06 -0500

    test: añadir funcion de calculo de promedio

--- t1.py
+++ t1.py
@@ -34,15 +34,48 @@
 
 # print(f"Perímetro: {rect.perimetro()}")
 
-def contar_vocales(texto):
-    vocales = "aeiouáéíóú"
-    contador = 0
-    for letra in texto.lower():
-        if letra in vocales:
-            contador += 1
-    return contador #CONCHATUMARE IDENTACION
+# def contar_vocales(texto):
+#     vocales = "aeiouáéíóú"
+#     contador = 0
+#     for letra in texto.lower():
+#         if letra in vocales:
+#             contador += 1
+#     return contador #CONCHATUMARE IDENTACION
 
-frase = "Programar en Python es divertido"
-total_vocales = contar_vocales(frase)
-print(f"La frase tiene {total_vocales} vocales.")
+# frase = "Programar en Python es divertido"
+# total_vocales = contar_vocales(frase)
+# print(f"La frase tiene {total_vocales} vocales.")
 
+#Escribe una funcion llamada calcular promedio que 
+# reciba una lista  de calificaciones (numeros), las notas 
+#enviadas en la lista deben ser: 12, 15, 18, 10 y 17
+#ademas retorne el promedio. Si la lista esta vacia, debe retornar 0.
+#Salida esperada: promedio = 14.40.
+
+#PASO 3: Calcular el promedio, desempaquetado de lista
+def calcu_prom(*numeros):
+    a = len(*numeros)
+    prom = sum(*numeros) / a
+    return prom
+
+#PASO 1: Bucle para ingresar notas enteras y presionar q para salir del bucle
+while True:
+    dato1 = input("Ingresa tus notas o presiona 'q' para salir: ")
+
+    if dato1.lower() == 'q':
+        print("Has salido.")
+        ente = []
+        break
+
+    try:
+        ente = [int(x) for x in dato1.split()]
+        if not ente:
+            raise ValueError
+        print(f"Tus notas son las siguientes: {ente}")
+        break
+    except ValueError:
+        print("Ingresa un número entero o presiona 'q' para salir.")
+
+if ente:
+    res = calcu_prom(ente)
+    print(f"El promedio de tus notas es {res}")
